ead98ff03bbcb135c541a776706273f8f057bd35
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-16 14:15:28 +0100

    IntroduceYourself

---
+++ src/com/greenfoxacademy/IntroduceYourself.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Holczer János");
+        System.out.println("53");
+        System.out.println("1.70");
+    }
+}
