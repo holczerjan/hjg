649ae25a602cbe28c852585ed8cb95ed667f1d0b
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-16 23:42:19 +0100

    Swap

--- src/com/greenfoxacademy/Swap.java
+++ src/com/greenfoxacademy/Swap.java
@@ -6,8 +6,9 @@ public class Swap {
         // Az a értéke legyen az, ami a b változóban van és fordítva.
         int a = 123;
         int b = 526;
-        a = 526;
-        b = 123;
+        a = a + b;
+        b = a - b;
+        a = a - b;
         System.out.println(a);
         System.out.println(b);
 
---
+++ src/com/greenfoxacademy/VariableMutation.java
@@ -0,0 +1,40 @@
+package com.greenfoxacademy;
+
+public class VariableMutation {
+    public static void main(String[] args) {
+        // Növeld meg az "a" változó értékét 10-zel
+        int a = 3;
+        a += 10;
+        System.out.println(a);
+
+        // Csökkentsd a "b" változó értékeét 7-tel
+        int b = 100;
+        b -= 7;
+        System.out.println(b);
+
+        // A c értéke legyen dupla akkora
+        int c = 44;
+        c *= 2;
+        System.out.println(c);
+
+        // Oszd el a d-t 5-tel
+        int d =125;
+        d /= 5;
+        System.out.println(d);
+
+        // Emeld köbre az e változó értékét
+        int e = 8;
+        e *= 8;
+        System.out.println(e);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
