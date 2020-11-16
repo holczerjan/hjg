9f9e42e9395c1dc44396e21f7fbe45a3263a020c
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-16 17:24:08 +0100

    DefineBasicInfo

---
+++ src/com/greenfoxacademy/DefineBasicInfo.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Hozz létre pár dolgot változóként és utána írasd ki az értékeiket:
+        // - A neved karakterláncként (string)
+        // - Korod egész számként (integer)
+        // - Magasságod méterben törtszámként (float / double)
+        // - Házas vagy-e logikai értékként (boolean)
+        String name = "Holczer János";
+        int age = 53;
+        double height = 1.7;
+        boolean married = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(married);
+
+    }
+}
