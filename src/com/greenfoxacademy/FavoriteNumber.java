ac785f908dffdc37bcbca26ac4045f14880a5821
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-16 16:08:49 +0100

    FavoriteNumber

---
+++ src/com/greenfoxacademy/FavoriteNumber.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        // Írd át a fenti számot a kedvenc számoddá
+        // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
+        // Kiíratáskor használd fel a favoriteNumber változót
+        int favoriteNumber = 42;
+        System.out.println("A kedvenc számom: " +favoriteNumber);
+
+    }
+}
