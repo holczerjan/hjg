d4a3aaa27fc0779c052e64d1ac9eaad2a460eba9
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-16 14:06:11 +0100

    HelloOthers

---
+++ src/com/greenfoxacademy/HelloOthers.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+        //
+        // Helló Eszter!
+        // Helló Maris!
+        // Helló Józsi!
+        System.out.println("Helló József!");
+        System.out.println("Helló Adrienn!");
+        System.out.println("Helló Sándor!");
+    }
+}
