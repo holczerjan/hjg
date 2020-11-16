3993848e9bdd606216309cd1ccf9c848795756e8
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-16 16:56:56 +0100

    Bmi

---
+++ src/com/greenfoxacademy/Bmi.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy;
+
+public class Bmi {
+    public static void main(String[] args) {
+        // Számíttasd és írasd ki a testtömeg-indexet (BMI)
+        // a változókat használva
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+        System.out.println(heightInM / (massInKg * massInKg) );
+    }
+}
