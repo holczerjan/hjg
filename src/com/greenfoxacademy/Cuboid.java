c9614e5de0d6dee865f9b6598c568200c436b250
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-17 08:59:34 +0100

    Cuboid

---
+++ src/com/greenfoxacademy/Cuboid.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy;
+
+public class Cuboid {
+    public static void main(String[] args) {
+        // Írj egy programot, ami egy téglatest 3 oldalát tartalmazza változókban (doubles)
+        // A program írja ki a felületét és térfogatát a testnek ilyen formátumban:
+        //
+        // Felület: 600
+        // Térfogat: 1000
+
+       /* double a = 10;
+        double b = 10;
+        double c = 10;
+        double bodySurface = 2 * (a * b + b * c + a * c);
+        double bodyVolume = a * b * c;
+        System.out.println("Felület : " + bodySurface);
+        System.out.println("Térfogat : " + bodyVolume);
+
+        */
+       double a;
+       double b;
+       double c;
+       double bodySurface = 2 * (a * b + b * c + a * c);
+       double bodyVolume = a * b * c;
+       System.out.println("Felület : " + bodySurface);
+       System.out.println("Térfogat : " + bodyVolume);
+
+    }
+}
