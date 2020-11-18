6351d9c61cdbff37235dfab35c58ce4ab0d95928
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-18 13:33:45 +0100

    Third

---
+++ src/com/greenfoxacademy/AverageOfInput.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy;
+
+import java.util.Scanner;
+
+public class AverageOfInput {
+    public static void main(String[] args) {
+        // Írj egy programot, ami 5 egész számot kér be egymás után,
+        // majd kiírja a számok összegét és átlagát a következő formátumban:
+        //
+        // Összeg: 22, Átlag: 4.4
+
+        int userInput = 0;
+        System.out.println("Adj meg egy egész számot!");
+        Scanner scanner = new Scanner(System.in);
+        userInput = scanner.nextInt();
+
+
+
+
+
+    }
+}
---
+++ src/com/greenfoxacademy/Third.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy;
+
+public class Third {
+    public static void main(String[] args) {
+
+// - Készíts egy `numbers` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[4, 5, 6, 7]`
+// - Írasd ki a konzolra a `numbers` nevű tömb harmadik elemét
+
+        int[] third = {4, 5, 6, 7};
+        System.out.println(third[2]);
+
+
+    }
+}
