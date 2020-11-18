f62a36f39f36370948cafe12a6cd0469ada2d19a
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-18 19:24:49 +0100

    PrintAll

---
+++ src/com/greenfoxacademy/PrintAll.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy;
+
+import java.util.Arrays;
+
+public class PrintAll {
+    public static void main(String[] args) {
+
+
+// - Készíts egy `numbers` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[4, 5, 6, 7]`
+// - Írasd ki a konzolra a `numbers` nevű tömb összes elemét
+
+        int[] numbers = {4, 5, 6, 7};
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
