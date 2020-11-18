1de96484e6c74029d48ddbfc581f0cdcfc0ad210
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-18 19:17:11 +0100

    IncrementElement

---
+++ src/com/greenfoxacademy/IncrementElement.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy;
+
+public class IncrementElement {
+    public static void main(String[] args) {
+
+
+// - Készíts egy `numbers` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[1, 2, 3, 4, 5]`
+// - Növeld a harmadik elem értékét (pl.: eggyel)
+// - Írasd ki a konzolra a `numbers` nevű tömb harmadik elemét
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        numbers[2] ++;
+        System.out.println(numbers[2]);
+
+
+    }
+}
