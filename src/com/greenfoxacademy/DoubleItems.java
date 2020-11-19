50ff5745eb08a017bba2bf9430d4c41329970a42
holczerjan <72969402+holczerjan@users.noreply.github.com>
2020-11-19 14:43:57 +0100

    DoubleItems

---
+++ src/com/greenfoxacademy/DoubleItems.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy;
+
+import java.util.Arrays;
+
+public class DoubleItems {
+    public static void main(String[] args) {
+
+
+// - Készíts egy `numList` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[3, 4, 5, 6, 7]`
+// - Duplázd meg minden elem értéket a tömbben
+
+        int[] numlist = {3, 4, 5, 6, 7};
+        for (int i = 0; i <= 4; i++){
+            numlist[i] *= 2;
+        }
+
+        
+    }
+
+
+}
+
